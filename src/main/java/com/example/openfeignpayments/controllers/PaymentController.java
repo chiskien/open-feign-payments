3ba194e30d1bcab26918e1a6824ece49abd8ce50
ChisKien <chiskien03214@gmail.com>
2023-05-06 11:13:08 +0700

    Finish implements controller and proxy open feign

--- src/main/java/com/example/openfeignpayments/controllers/PaymentController.java
+++ src/main/java/com/example/openfeignpayments/controllers/PaymentController.java
@@ -2,10 +2,12 @@ package com.example.openfeignpayments.controllers;
 
 import com.example.openfeignpayments.models.Payment;
 import com.example.openfeignpayments.proxies.PaymentProxy;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -21,4 +23,10 @@ public class PaymentController {
         String requestId = UUID.randomUUID().toString();
         return paymentProxy.createPayment(requestId, payment);
     }
+
+    @GetMapping("/payment")
+    public List<Payment> getAll() {
+        return paymentProxy.getAll();
+    }
+
 }
--- src/main/java/com/example/openfeignpayments/proxies/PaymentProxy.java
+++ src/main/java/com/example/openfeignpayments/proxies/PaymentProxy.java
@@ -2,12 +2,19 @@ package com.example.openfeignpayments.proxies;
 
 import com.example.openfeignpayments.models.Payment;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "payment", url = "${name.service.url")
+import java.util.List;
+
+@FeignClient(name = "payment", url = "${name.service.url}")
 public interface PaymentProxy {
     @PostMapping("/payment")
-    Payment createPayment(@RequestHeader String requestId, @RequestBody Payment payment);
+    Payment createPayment(@RequestHeader String requestId,
+                          @RequestBody Payment payment);
+
+    @GetMapping("/payment")
+    List<Payment> getAll();
 }
