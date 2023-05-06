f06b63c9c353c719a45e479f7bd8f164f0e4077e
ChisKien <chiskien03214@gmail.com>
2023-05-06 10:09:52 +0700

    Call API by using Spring Cloud OpenFeign

---
+++ src/main/java/com/example/openfeignpayments/OpenFeignPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.openfeignpayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeignPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignPaymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/openfeignpayments/OpenFeignPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openfeignpayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFeignPaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
